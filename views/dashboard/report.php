34ce253384f0a4f04c8fb1cf12291c83aeabdc50
3steak <fc.barcelone94@hotmail.fr>
2023-02-28 21:48:57 +0100

    report comment delete

--- views/dashboard/report.php
+++ views/dashboard/report.php
@@ -61,44 +61,6 @@
                         </div>
                     </div>
                 </div>
-                <div class="container-fluid col-lg-6  my-3 cardUserContent">
-                    <div class="row">
-                        <!--  Div ReportContent -->
-                        <div class="reportContent alert alert-danger" role="alert">
-                            <h2 class=" reportTitle">Signalement de "userName2"</h2>
-                            <p class="">"date"</p>
-                            <p class=" text-center"> "Ce commentaire est une honte"
-                            </p>
-                        </div>
-                        <div class="container-fluid  mb-3 cardUserContent">
-                            <div class="row">
-                                <div class="card d-flex  gap-2 text-bg-dark pb-3">
-                                    <div class="col-12 p-2 ">
-                                        <div class="list-group-item d-flex align-items-center ">
-                                            <a href="profilUser.html"><img class="img-fluid miniProfilUser my-auto" src="/public/assets/img/miniProfilUser.png" alt="photo profil utilisateur">
-                                                <a href="#" class="text-decoration-none text-white p-2 ">
-                                                    Profil Name
-                                                </a>
-                                        </div>
-                                        <div>
-                                            <p>Commentaire du DATE </p>
-                                        </div>
-                                    </div>
-                                    <div class="input-group">
-
-                                        <textarea class="form-control" value="">WOW AMAZING !</textarea>
-                                    </div>
-                                    <div class="col-12 d-flex gap-2"> <button type="button" class="btn btn-primary w-50 mx-auto">C'EST OK</button>
-
-                                        <!-- IF DELETE ADD MODAL WITH MAILUSER -->
-                                        <button type="button" class="btn btn-primary w-50 mx-auto" data-bs-toggle="modal" data-bs-target="#deleteMailModal" data-bs-whatever="USER2">SUPPRIMER</button>
-                                    </div>
-
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
 
             </div>
 
