86943b2c95475897cd0f50bc7ba1b632e906069b
3steak <fc.barcelone94@hotmail.fr>
2023-03-05 23:28:08 +0100

    ajout logo nul

--- views/home.php
+++ views/home.php
@@ -7,14 +7,7 @@
           <div class="container-fluid my-5 bg-white bg-opacity-75 ">
               <div class="row">
                   <div class="col">
-                      <h1 class="text-center p-5 text-dark">FUTUR LOGO ICI</h1>
-                  </div>
-              </div>
-          </div>
-          <div class="container-fluid my-5 bg-white bg-opacity-75 ">
-              <div class="row">
-                  <div class="col">
-                      <h1 class="text-center p-5 text-dark">FUTUR LOGO ICI</h1>
+                      <h1 class="text-center p-5 text-dark"><img src="/public/assets/img/logo-removebg-.png" class="logoHome" alt=""></h1>
                   </div>
               </div>
           </div>
