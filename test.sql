74e3369ffa19fc461f925b4b83c93d7453afd0e1
3steak <fc.barcelone94@hotmail.fr>
2023-03-05 17:07:44 +0100

    ajout sql

--- test.sql
+++ test.sql
@@ -1,67 +1,243 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Mar 05, 2023 at 04:06 PM
+-- Server version: 8.0.30
+-- PHP Version: 8.2.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
-CREATE TABLE users(
-   id INT AUTO_INCREMENT,
-   firstname VARCHAR(50)  NOT NULL,
-   lastname VARCHAR(50)  NOT NULL,
-   pseudo VARCHAR(50)  NOT NULL,
-   password VARCHAR(255)  NOT NULL,
-   email VARCHAR(150)  NOT NULL,
-   birthday DATE NOT NULL,
-   created_at DATETIME,
-   updated_at DATETIME,
-   deleted_at DATETIME,
-   admin BOOLEAN NOT NULL,
-   profil_img_name TINYINT,
-   id_categories INT NOT NULL,
-   PRIMARY KEY(id),
-   FOREIGN KEY(id_categories) REFERENCES categories(id)
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-CREATE TABLE publications(
-   id INT AUTO_INCREMENT,
-   title VARCHAR(100)  NOT NULL,
-   description VARCHAR(300) ,
-   deleted_at DATETIME,
-   created_at DATETIME,
-   image_name VARCHAR(100) ,
-   marker_longitude DECIMAL(9,7)  ,
-   marker_latitude DECIMAL(9,7)  ,
-   validated_at DATETIME,
-   town VARCHAR(100)  NOT NULL,
-   report BOOLEAN NOT NULL,
-   likes SMALLINT,
-   id_categories INT,
-   id_users INT NOT NULL,
-   PRIMARY KEY(id),
-   FOREIGN KEY(id_categories) REFERENCES categories(id),
-   FOREIGN KEY(id_users) REFERENCES users(id)
-);
+--
+-- Database: `testxtrem`
+--
 
-CREATE TABLE comments(
-   id INT AUTO_INCREMENT,
-   description VARCHAR(500)  NOT NULL,
-   validated_at DATETIME,
-   created_at DATETIME,
-   deleted_at DATETIME,
-   report TINYINT NOT NULL,
-   id_users INT NOT NULL,
-   id_publications INT NOT NULL,
-   PRIMARY KEY(id),
-   FOREIGN KEY(id_users) REFERENCES users(id),
-   FOREIGN KEY(id_publications) REFERENCES publications(id)
-);
+-- --------------------------------------------------------
 
-CREATE TABLE favorites(
-   id INT AUTO_INCREMENT,
-   id_publications INT NOT NULL,
-   id_users INT NOT NULL,
-   PRIMARY KEY(id),
-   FOREIGN KEY(id_publications) REFERENCES publications(id),
-   FOREIGN KEY(id_users) REFERENCES users(id)
-);
-CREATE TABLE categories(
-   id INT AUTO_INCREMENT,
-   name VARCHAR(50) ,
-   PRIMARY KEY(id)
-);
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int NOT NULL,
+  `name` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`) VALUES
+(1, 'Skate'),
+(2, 'Roller'),
+(3, 'Bmx'),
+(4, 'Surf');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comments`
+--
+
+CREATE TABLE `comments` (
+  `id` int NOT NULL,
+  `description` varchar(500) COLLATE utf8mb4_general_ci NOT NULL,
+  `validated_at` datetime DEFAULT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `deleted_at` datetime DEFAULT NULL,
+  `report` tinyint NOT NULL,
+  `id_users` int NOT NULL,
+  `id_publications` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `favorites`
+--
+
+CREATE TABLE `favorites` (
+  `id` int NOT NULL,
+  `id_publications` int NOT NULL,
+  `id_users` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `publications`
+--
+
+CREATE TABLE `publications` (
+  `id` int NOT NULL,
+  `title` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `description` varchar(300) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `marker_longitude` decimal(9,7) DEFAULT NULL,
+  `marker_latitude` decimal(9,7) DEFAULT NULL,
+  `validated_at` datetime DEFAULT NULL,
+  `town` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `report` tinyint(1) DEFAULT NULL,
+  `likes` smallint DEFAULT NULL,
+  `id_categories` int DEFAULT NULL,
+  `id_users` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `publications`
+--
+
+INSERT INTO `publications` (`id`, `title`, `description`, `deleted_at`, `created_at`, `marker_longitude`, `marker_latitude`, `validated_at`, `town`, `report`, `likes`, `id_categories`, `id_users`) VALUES
+(3, 'Superbe spot gngngn', 'Spot rocheux !', NULL, '2023-03-04 12:28:01', '50.0660720', '1.3892870', NULL, 'Mers-les-Bains', NULL, NULL, 4, 1),
+(4, 'Skatepark bien null ', 'une rape a fromage le truc ! ', NULL, '2023-03-04 12:28:01', '2.0810700', '50.0148960', NULL, 'Flixecourt', NULL, NULL, 1, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int NOT NULL,
+  `firstname` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `lastname` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `pseudo` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(150) COLLATE utf8mb4_general_ci NOT NULL,
+  `birthday` date NOT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  `admin` tinyint(1) DEFAULT NULL,
+  `id_categories` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `firstname`, `lastname`, `pseudo`, `password`, `email`, `birthday`, `created_at`, `updated_at`, `deleted_at`, `admin`, `id_categories`) VALUES
+(1, 'Jean', 'Bon', 'Jeanbonsiisouioui', 'oioioioioio', 'jean@gmail.com', '1999-12-09', '2023-03-04 12:26:09', NULL, NULL, 0, 1),
+(2, 'Aqueuse', 'Cat', 'Acqueuse', 'IOIOIOIO', 'aqueuse@gmail.com', '1994-02-09', '2023-03-04 12:27:00', NULL, NULL, 0, 1),
+(3, 'Sakai', 'Asinbit', 'Sakai', 'ouioui', 'sakai@gmail.com', '1995-10-12', '2023-03-04 12:27:45', NULL, NULL, 1, 3);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_users` (`id_users`),
+  ADD KEY `id_publications` (`id_publications`);
+
+--
+-- Indexes for table `favorites`
+--
+ALTER TABLE `favorites`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_publications` (`id_publications`),
+  ADD KEY `id_users` (`id_users`);
+
+--
+-- Indexes for table `publications`
+--
+ALTER TABLE `publications`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_categories` (`id_categories`),
+  ADD KEY `id_users` (`id_users`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_categories` (`id_categories`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `favorites`
+--
+ALTER TABLE `favorites`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `publications`
+--
+ALTER TABLE `publications`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `comments`
+--
+ALTER TABLE `comments`
+  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`id_users`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`id_publications`) REFERENCES `publications` (`id`);
+
+--
+-- Constraints for table `favorites`
+--
+ALTER TABLE `favorites`
+  ADD CONSTRAINT `favorites_ibfk_1` FOREIGN KEY (`id_publications`) REFERENCES `publications` (`id`),
+  ADD CONSTRAINT `favorites_ibfk_2` FOREIGN KEY (`id_users`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `publications`
+--
+ALTER TABLE `publications`
+  ADD CONSTRAINT `publications_ibfk_1` FOREIGN KEY (`id_categories`) REFERENCES `categories` (`id`),
+  ADD CONSTRAINT `publications_ibfk_2` FOREIGN KEY (`id_users`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`id_categories`) REFERENCES `categories` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
