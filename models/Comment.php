8c2ce85181347a3b11c06487d40177f461755d9c
3steak <fc.barcelone94@hotmail.fr>
2023-03-07 15:33:23 +0100

    model et class ok

--- models/Comment.php
+++ models/Comment.php
@@ -42,13 +42,79 @@ class Comment
         return $this->idPublications;
     }
 
-    public static function get(int $idUsers = null, int $idPublications = null)
+    /** Methode get permet de retourner les commentaires à valider si 0 paramètres rentrés,
+     * Si param = $idUser la méthode retourne les commentaires validés de l'user 
+     * Si param = $idPublication la méthode retourne les commentaires validés de la publication
+     * get
+     *
+     * @param  mixed $idUser
+     * @param  mixed $idPublication
+     * @return array
+     */
+    public static function get(int $idUser = null, int $idPublication = null): array
     {
-        if ($idUsers) {
-            #return commentaire de l'user
-        } elseif ($idPublications) {
-            #return commentaire where 
+        if ($idUser) {
+            #return commentaires de l'user
+            $sql = 'SELECT `id` as `commentId`, `description`, `validated_at`, `created_at`, `idUsers`, `idPublications`
+                    FROM `comments` 
+                    WHERE (`validated_at` is not null) AND `idUsers` = :id ;';
+            $sth = Database::connect()->prepare($sql);
+            $sth->bindValue(':id', $idUser, PDO::PARAM_INT);
+        } elseif ($idPublication) {
+            #return commentaires de la publication
+            $sql = 'SELECT `id` as `commentId`, `description`, `validated_at`, `created_at`, `idUsers`, `idPublications`
+            FROM `comments` 
+            WHERE (`validated_at` is not null) AND `idUsers` = :idPublication ;';
+            $sth = Database::connect()->prepare($sql);
+            $sth->bindValue(':id', $idPublication, PDO::PARAM_INT);
+        } else {
+            #return commentaires non validés pour modération
+            $sql = 'SELECT `id` as `commentId`, `description`, `validated_at`, `created_at`, `idUsers`, `idPublications`
+            FROM `comments` 
+            WHERE (`validated_at` is null) ;';
+            $sth = Database::connect()->prepare($sql);
+            $sth->bindValue(':id', $idPublication, PDO::PARAM_INT);
         }
+        $sth->execute();
+        $comments = $sth->fetchAll();
+        return $comments;
+    }
+
+    /** Methode qui permet d'ajouter un commentaire
+     * addComments
+     *
+     * @return bool
+     */
+    public function addComments(): bool
+    {
+        $sql = "INSERT INTO `comments` (`idPublications`, `idUsers`,`description`)
+                VALUES (:idPublications, :idUsers, :description);";
+
+        $sth = Database::connect()->prepare($sql);
+        $sth->bindValue(':idPublications', $this->idPublications, PDO::PARAM_INT);
+        $sth->bindValue(':idPublications', $this->idPublications, PDO::PARAM_INT);
+        $sth->bindValue(':description', $this->description, PDO::PARAM_STR);
+
+        $sth->execute();
+        $result = $sth->rowCount();
+        return ($result > 0) ? true : false;
+    }
+
+
+    /** Methode qui permet de supprimer un commentaire
+     * delete
+     *
+     * @param  mixed $idComments
+     * @return bool
+     */
+    public static function delete(int $idComments): bool
+    {
+        $sql = 'DELETE FROM `comments` WHERE id = :id;';
+        $sth = Database::connect()->prepare($sql);
+        $sth->bindValue(':id', $idComments, PDO::PARAM_INT);
+        $sth->execute();
+        $result = $sth->rowCount();
+        return ($result > 0) ? true : false;
     }
 
     // END CLASS 
