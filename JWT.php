b7eecd2ea813bf70c25b5115bfc9b4df03a18e5f
3steak <fc.barcelone94@hotmail.fr>
2023-03-28 11:57:06 +0200

    ajout explication JWT

---
+++ JWT.php
@@ -0,0 +1,28 @@
+<?php
+
+// updated_at : ON CASCADE UPDATED AT CURRENTTIMESTAMP
+
+// UN DOSSIER PAR ENTITE DANS UPLOADS 
+
+//  SERVER STATELESS, SANS GARDER EN MEMOIRE ENTRE LES REQUETES
+//  JWT TOKKEN de 3 blocs de texte séparé par des points 
+//  encodé en base64 qui permet de transiter en HTTP 
+//  HEADER, BODY, SIGNATURE
+
+//  HEADER précise l'algo utilisé pour la signature
+// BODY  Désigne le json pour définir id name role exp.. 
+//  DEFINIR DATE DE CREATION ET EXPRIRATION DU TOKEN ( exp = expiration)
+//  SIGNATURE PERMET DE VALIDER LE TOKKEN 
+
+// ----------- PROCESS ---------------
+//  RECUPERATION DES DONNES + FILTRES
+
+$password1 = filter_input(INPUT_POST, 'password');
+$password2 = filter_input(INPUT_POST, 'password');
+if ($password1 != $password2) {
+    # code... message erreur
+}
+password_hash($password1, PASSWORD_DEFAULT);
+if (empty($error)) {
+    # code...
+}
