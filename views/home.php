a8ca8d3d2a15ed9fb777c8d0e5db70cc9acc0eaf
3steak <fc.barcelone94@hotmail.fr>
2023-04-04 16:59:42 +0200

    ajout img

--- views/home.php
+++ views/home.php
@@ -6,8 +6,11 @@
       <main>
           <div class="container-fluid my-5 bg-white bg-opacity-75 ">
               <div class="row">
-                  <div class="col">
-                      <h1 class="text-center p-5 text-dark"><img src="/public/assets/img/logo-removebg-.png" class="logoHome" alt=""></h1>
+                  <div class="col-12">
+                      <h1 class="text-center text-dark"><img src="/public/assets/img/logo-removebg-.png" class="logoHome" alt="Logo Home"></h1>
+                  </div>
+                  <div class="col-12">
+                      <h1 class="text-center text-dark"><img src="/public/assets/img/XtremSpot.png" class="Slogan" alt="Slogan"></h1>
                   </div>
               </div>
           </div>
