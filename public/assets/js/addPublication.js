68e4f9bebb41b187d2207b3f62adffc75d50e140
3steak <fc.barcelone94@hotmail.fr>
2023-04-12 16:59:55 +0200

    oui

--- public/assets/js/addPublication.js
+++ public/assets/js/addPublication.js
@@ -18,78 +18,84 @@ $(document).ready(function () {
 
         // this refers to zipcode input 
         let code = $(this).val();
-        // FAIRE LA PREMIERE REQUETE AVEC DATAGOUVAPI
+        // URL for api with code and apiKey added
         let url = 'http://api.openweathermap.org/geo/1.0/zip?zip=' + code + ',FR&appid=' + apiKey;
         // Send request to API
-        fetch(url, { method: 'get' }).then(response => response.json()).then(result => {
-            $(city).find('option').remove();
-            if (result.code !== '') {
-                $(city).append('<option value ="' + result.name + '">' + result.name + '</option>');
-                //  refaire une requete API a  openweather avec result.name
-                function init() {
-                    if (code.match(regexZipcode) === null) {
-                        $(errorMessage).text('Rentrer un code postal').show();
-                        result.lat = 48.8534;
-                        result.lon = 2.3488;
-                        setTimeout(() => {
-                            $(errorMessage).text('').hide();
-                        }, "2000");
+        fetch(url, { method: 'get' })
+            .then(response => response.json())
+            .then(result => {
+                $(city).find('option').remove();
+                // if result != empty, add with append <option value... 
+                if (result.code !== '') {
+                    $(city).append('<option value ="' + result.name + '">' + result.name + '</option>');
+                    //  refaire une requete API a  openweather avec result.name
+                    function init() {
+                        if (code.match(regexZipcode) === null) {
+                            $(errorMessage).text('Rentrer un code postal').show();
+                            result.lat = 48.8534;
+                            result.lon = 2.3488;
+                            setTimeout(() => {
+                                $(errorMessage).text('').hide();
+                            }, "2000");
+                        }
+                        // Création de mon ping personnalisé
+
+                        var myIcon = L.icon({
+                            iconUrl: '/../public/assets/img/surf.png',
+                            iconSize: [40, 40],
+                            popupAnchor: [0, -30],
+                        });
+                        // Coordonnés de ma ville
+                        const town = {
+                            lat: result.lat,
+                            lng: result.lon
+                        }
+                        // lvl du zoom sur la ville 
+                        const zoomLevel = 12;
+
+                        //  "L" est un objet
+                        // J'utilise ses methodes 
+                        map = L.map('mapid').setView([town.lat, town.lng], zoomLevel);
+
+                        const mainLayer = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                            maxZoom: 19,
+                            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+                        });
+                        // AJout du layer a la map(MAPID dans le html)
+                        mainLayer.addTo(map);
+
+
+                        // //  ajout marker
+                        var marker = L.marker([], { icon: myIcon });
+
+                        // Le titre de publication
+                        let content = " Ton titre se retrouvera ici ";
+
+                        function onMapClick(e) {
+                            marker
+                                .setLatLng(e.latlng)
+                                .bindPopup("<small>" + content + "<br>" + e.latlng + "</small>")
+                                .addTo(map);
+                            $(hiddenInput).val(e.latlng);
+                        }
+                        map.on('click', onMapClick);
+                        // fin fonction init
                     }
-                    // Création de mon ping personnalisé
+                    init();
 
-                    var myIcon = L.icon({
-                        iconUrl: '/../public/assets/img/surf.png',
-                        iconSize: [40, 40],
-                        popupAnchor: [0, -30],
-                    });
-                    // Coordonnés de ma ville
-                    const town = {
-                        lat: result.lat,
-                        lng: result.lon
+                    // if result empty show error message
+                } else {
+                    if ($(zipcode).val()) {
+                        $(errorMessage).text('Aucune commmune avec ce code postal.').show();
                     }
-                    // lvl du zoom sur la ville 
-                    const zoomLevel = 12;
-
-                    //  "L" est un objet
-                    // J'utilise ses methodes 
-                    map = L.map('mapid').setView([town.lat, town.lng], zoomLevel);
-
-                    const mainLayer = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                        maxZoom: 19,
-                        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-                    });
-                    // AJout du layer a la map(MAPID dans le html)
-                    mainLayer.addTo(map);
-
-
-                    // //  ajout marker
-                    var marker = L.marker([], { icon: myIcon });
-
-                    // Le titre de publication
-                    let content = " Ton titre se retrouvera ici ";
-
-                    function onMapClick(e) {
-                        marker
-                            .setLatLng(e.latlng)
-                            .bindPopup("<small>" + content + "<br>" + e.latlng + "</small>")
-                            .addTo(map);
-                        $(hiddenInput).val(e.latlng);
+                    else {
+                        $(errorMessage).text('').hide();
                     }
-                    map.on('click', onMapClick);
-                    // fin fonction init
                 }
-                init();
-
-            } else {
-                if ($(zipcode).val()) {
-                    $(errorMessage).text('Aucune commmune avec ce code postal.').show();
-                }
-                else {
-                    $(errorMessage).text('').hide();
-                }
-            }
-        }).catch(err => {
-            $(city).find('option').remove();
-        });
+            })
+            // if catch errors, remove town
+            .catch(err => {
+                $(city).find('option').remove();
+            });
     });
 });
